acb35484998c501327193661c80783fc0dc41718
Adil Abed <dr.sivana@protonmail.com>
2017-11-17 17:09:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
 def call(even)
   resp = Rack::Response.new
   time1 = Time.now
-  if time1.hour.between(0,12)
+  if time1.hour.between?(0,12)
     resp.write "Good Morning!"
   else
     resp.write "Good Afternoon!"
